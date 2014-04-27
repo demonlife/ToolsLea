5a60c5e4079e9df41d7c546e00a60578f4d8468c
demonlife <demonlife2013@gmail.com>
2014-04-27 11:26:39 +0800

    modify some files

--- Emacs/EmacsDotFile/emacs.d/base_conf/base.el
+++ Emacs/EmacsDotFile/emacs.d/base_conf/base.el
@@ -1,3 +1,7 @@
+;;启动时的大小和屏幕的中的位置
+(setq default-frame-alist
+    '((height . 35)(width . 100)(menubar-lines . 20)(tool-bar-lines . 0)))
+
 ;;显示时间
 (display-time)
 (display-time-mode 1);;启用时间显示设置，在minibuffer上面的那个杠上
@@ -11,16 +15,18 @@
 
 ;;显示列号
 (setq column-number-mode t)
+;; 显示行号
+(global-linum-mode 1)
+(setq linum-format "%d")
 
 ;; 不要在鼠标点击的地方插入剪贴板的内容
 (setq mouse-yank-at-point t)
 
 ;; 设置一个很大的kill ring
-(setq kill-ring-max 200)
+(setq kill-ring-max 500)
 
 ;; 将fill-column设为60
 (setq default-fill-column 60)
-
 
 ;;不产生备份文件
 (setq make-backup-files nil)
@@ -37,6 +43,9 @@
 
 ;; 去掉滚动条
 (set-scroll-bar-mode nil)
+;(scroll-bar-mode nil);去掉滚动条
+;(scroll-bar-mode 0);
+
 ;;关闭开启画面
 (setq inhibit-startup-message t)
 ;;禁用工具栏
@@ -70,7 +79,13 @@
 (mouse-avoidance-mode 'animate)
 
 ;; 标题显示有用信息
-(setq frame-title-format "emacs@%b")
+(setq frame-title-format 
+      '("%S" (buffer-file-name "%f"
+                               (dired-directory dired-directory "%b"))))
+
+;;用于区分打开相同文件名时无法区分的情况下
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
 
 ;; 可以直接打开和显示图片
 (auto-image-file-mode)
@@ -88,3 +103,103 @@
 ;; 让dired可以递归的拷贝和删除目录
 (setq dired-recursive-copies 'top)
 (setq dired-recursive-deletes 'top)
+
+;支持emacs和外部程序的粘贴
+(setq x-select-enable-clipboard t)
+
+;;用aspell程序替代ispell
+(setq-default ispell-program-name "aspell")
+
+(setq user-full-name "demonlife");设置全名
+(setq user-mail-address "demonlife2013@gmail.com");设置个人邮箱
+
+;;代码缩进
+(require 'electric)
+(electric-indent-mode +1)
+
+;;高亮显示当前行
+(global-hl-line-mode t)
+
+;;默认使用Shitf+上下左右键来切换各个窗口
+(windmove-default-keybindings)
+
+;;窗口最大化以后调用另一个buffer
+(setq winner-dont-bind-my-keys t)
+(winner-mode t)
+(global-set-key (kbd "C-c b") 'winner-undo)
+(global-set-key (kbd "C-c f") 'winner-redo)
+
+;;矩形选择功能
+(cua-mode t)
+
+;; 使用google搜索某个词
+(defun google ()
+  "Google the selected region if any, display a query prompt otherwise."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (url-hexify-string (if mark-active
+         (buffer-substring (region-beginning) (region-end))
+         (read-string "Google: "))))))
+
+(global-set-key (kbd "C-c g") 'google)
+
+;;;Open File in External Program START
+(defun open-with ()
+  "Simple function that allows us to open the underlying
+file of a buffer in an external program."
+  (interactive)
+  (when buffer-file-name
+    (shell-command (concat
+                    (if (eq system-type 'darwin)
+                        "open"
+                      (read-shell-command "Open current file with: "))
+                    " "
+                    buffer-file-name))))
+
+(global-set-key (kbd "C-c o") 'open-with)
+;;; END
+
+;; 编码设置 begin
+(set-language-environment 'Chinese-GB)
+;; default-buffer-file-coding-system变量在emacs23.2之后已被废弃，使用buffer-file-coding-system代替
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(setq-default pathname-coding-system 'euc-cn)
+(setq file-name-coding-system 'euc-cn)
+;; 另外建议按下面的先后顺序来设置中文编码识别方式。
+;; 重要提示:写在最后一行的，实际上最优先使用; 最前面一行，反而放到最后才识别。
+;; utf-16le-with-signature 相当于 Windows 下的 Unicode 编码，这里也可写成
+;; utf-16 (utf-16 实际上还细分为 utf-16le, utf-16be, utf-16le-with-signature等多种)
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+;(prefer-coding-system 'utf-16le-with-signature)
+(prefer-coding-system 'utf-16)
+;; 新建文件使用utf-8-unix方式
+;; 如果不写下面两句，只写
+;; (prefer-coding-system 'utf-8)
+;; 这一句的话，新建文件以utf-8编码，行末结束符平台相关
+(prefer-coding-system 'utf-8-dos)
+(prefer-coding-system 'utf-8-unix)
+;; 编码设置 end
+
+;; 对当前的文档添加注释
+(defun add-code-review-note ()
+   "Add note for current file and line number"
+   (interactive)
+   (let ((file-name (buffer-file-name))
+         (file-line (line-number-at-pos)))
+     (switch-to-buffer-other-window (get-buffer-create "NOTES"))
+     (goto-char (point-min))
+     (when (not (search-forward "-*- mode:compilation-shell-minor"
+                                nil t))
+       (compilation-shell-minor-mode 1)
+       (insert "-*- mode:compilation-shell-minor -*-\n\n"))
+     (goto-char (point-max))
+     (if (/= (current-column) 0)
+         (newline))
+     (insert file-name ":" (number-to-string file-line) ": ")))
+(global-set-key (kbd "C-x a") 'add-code-review-note)
